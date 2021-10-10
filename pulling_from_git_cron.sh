75da389174c1bb8c44b91c1ce8e09a7364a709ec
Vasyl Horbachuk <vasylh@playtika.com>
2021-10-10 15:16:43 +0000

    changed main title in index.html

---
+++ pulling_from_git_cron.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+#echo test >> /home/vhorbach/test.txt
+cd /home/vhorbach/material-design-template
+date && git pull --rebase git@github.com:v-horbachuk/material-design-template.git
